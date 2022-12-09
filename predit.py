c42b8da7a77a1904785adfef3fcf3a6afd0afe93
Viswateja Rayapaneni <viswatejaster@gmail.com>
2022-12-09 21:06:34 +0530

    Add files via upload

--- predit.py
+++ predit.py
@@ -10,7 +10,6 @@ import pickle
 
 def load_model():
     loaded_model = pickle.load(open(r"D:\MLOPs\finalized_model.sav", 'rb'))
-    print("loading model-----------------------------")
     return loaded_model
 
 def predict(loaded_model,data):
@@ -20,6 +19,6 @@ def predict(loaded_model,data):
 def input_req(data):
     model = load_model()
     response = predict(model,data)
-    return str(response[0])
+    return {"target_names":str(response[0])}
 
 
